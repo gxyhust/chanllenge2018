0202da6c5f1354f25fe73fddf7268209b3bbd3f3
gxyhust <gxyhust@163.com>
2018-01-27 15:42:56 +0800

    Update DataLoader.cpp

--- DataParserer/src/DataLoader.cpp
+++ DataParserer/src/DataLoader.cpp
@@ -1,33 +1,65 @@
-#ifndef DATALOADER_H
-#define DATALOADER_H
-#include <fstream>
-#include <iostream>
-#include <vector>
-#include "Message.h"
-#include <string>
+#define BUFFER 256
 
-#define FAKE "FAKE"
-#define RIGHTARROW ">"
-#define LEFTARROW "<"
-#define STARTENDFLAG "@"
-using namespace std;
-class DataLoader
+#include "DataLoader.h"
+#include "Util.h"
+
+char buffer[BUFFER];
+const string defaultFile = "\\..\\..\\..\\output\\output.wsd";
+
+vector<string> DataLoader::loadData(string &input)
 {
-    public:
-        virtual vector<string> loadData(string &input);
-        void checkContract();
-        string inputFile;
+}
 
-    protected:
-
-    private:
-
-};
-
-class FileLoader:public DataLoader
+vector<string> FileLoader::loadData(string &contractFilePath)
 {
-    public:
-        vector<string> loadData(string &contractFilePath);
-};
+    string strMsg;
+    vector<string> vStrMsg;
+    ifstream fin;
+    Util* u;
 
-#endif // DATALOADER_H
+    bool flag = false;
+    if (contractFilePath=="")
+        contractFilePath = defaultFile;
+    string fileAbsolutePath = Util::getProgramDir() + contractFilePath;
+    checkContract();
+    cout << "fileAbsolutePath="<<fileAbsolutePath<< endl;
+    if(!Util::checkFileExist(fileAbsolutePath))
+    {
+        cout << "tranfered file not exist" << endl;
+        exit(1);
+    }
+    fin.open(fileAbsolutePath);
+    if(!fin.is_open())
+    {
+        cout << "Error opening file" << endl;
+        exit(1);
+    }
+
+    while(!fin.eof())
+    {
+        fin.getline(buffer, BUFFER);
+        string strLine = buffer;
+        if ((strLine.find(FAKE) != string::npos) || (strLine.find(STARTENDFLAG) != string::npos))
+        {
+            flag = true;
+            if (strMsg.find(FAKE) != string::npos)
+            {
+                u->trim(strMsg);
+                vStrMsg.push_back(strMsg);
+            }
+            strMsg = strLine;
+        }
+        else if (flag && (strLine.find(STARTENDFLAG) == string::npos))
+        {
+            strMsg = strMsg + "\n" + strLine;
+        }
+    }
+    return vStrMsg;
+}
+
+
+
+void DataLoader::checkContract()
+{
+    //ctor;
+}
