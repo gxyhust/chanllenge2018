426eab5c70fd1d5428fc73fa8875cd082e32defa
gxyhust <gxyhust@163.com>
2018-01-27 15:43:14 +0800

    Update DataLoader.h

--- DataParserer/include/DataLoader.h
+++ DataParserer/include/DataLoader.h
@@ -1,17 +1,33 @@
 #ifndef DATALOADER_H
 #define DATALOADER_H
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include "Message.h"
 #include <string>
 
+#define FAKE "FAKE"
+#define RIGHTARROW ">"
+#define LEFTARROW "<"
+#define STARTENDFLAG "@"
 using namespace std;
 class DataLoader
 {
     public:
-        void LoadData();
-        void CheckContract();
+        virtual vector<string> loadData(string &input);
+        void checkContract();
+        string inputFile;
 
     protected:
 
     private:
+
+};
+
+class FileLoader:public DataLoader
+{
+    public:
+        vector<string> loadData(string &contractFilePath);
 };
 
 #endif // DATALOADER_H
