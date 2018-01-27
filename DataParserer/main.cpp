c3735266c493b5e405b09fd2923dd5c9178c6c17
gxyhust <gxyhust@163.com>
2018-01-27 15:46:52 +0800

    Update main.cpp

--- DataParserer/main.cpp
+++ DataParserer/main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-#include "DataLoader.h"
-#include "Analyser.h"
+#include <MsgStack.h>
+#include <Analyser.h>
+
 using namespace std;
 
 int main()
 {
     cout << "Hello world!" << endl;
+//    TcpStack tcpStack;
+//    MsgStack *msgStack = &tcpStack;
+//    msgStack->sendMsg("123");
+//    msgStack->recieveMsg();
+    Analyser analyser;
+    string contractFilePath = "\\..\\..\\..\\..\\output\\output.wsd";
+    analyser.publishMsgInfo(contractFilePath);
     return 0;
 }
