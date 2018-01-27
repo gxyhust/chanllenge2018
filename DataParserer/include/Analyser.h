15a73ed78388ff8b2c5a8f8df5acc3b86b83fe48
gxyhust <gxyhust@163.com>
2018-01-27 15:45:06 +0800

    Update Analyser.h

--- DataParserer/include/Analyser.h
+++ DataParserer/include/Analyser.h
@@ -1,19 +1,27 @@
 #ifndef ANALYSER_H
 #define ANALYSER_H
 #include <string>
-
-
+#include <iostream>
+#include <vector>
+#include "Message.h"
 using namespace std;
 class Analyser
 {
     public:
+//        string getSubscribeInfo(int subParam);
+//        string getFakeProcess();
+//        void getOperate(Message &msg, string &strProcessInfo);
+//        string getActivityModel(vector<Message> vMsg);
+//        void getProcessContent(Message &msg, string &strProcessInfo);
+//        string getProcess(vector<Message> vMsg);
 
-        void GetSubscribeInfoList(string subParamList,string subModelNameList);
-        void GetFakeProcess(string type,string subModelNameList);
-
-    protected:
+        vector<Message> getMsgProcess(const string &contractFilePath,const bool fake);
 
     private:
+        vector<Message> vMsg;
+        Message pushMsgPropertyValue(const string &strMsg);
+        vector<Message> publishMsgInfo(const string &contractFilePath);
+
 };
 
 #endif // ANALYSER_H
